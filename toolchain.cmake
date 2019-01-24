d1408c64b234d15a3e01ba40cf1b6dbee5a4d206
Vertmo <basile.pesin@gmail.com>
2019-01-24 10:23:38 +0100

    Optimized for size

--- toolchain.cmake
+++ toolchain.cmake
@@ -5,5 +5,12 @@ set(CMAKE_C_COMPILER "/usr/bin/arm-none-eabi-gcc")
 set(CMAKE_ASM_COMPILER ${CMAKE_C_COMPILER})
 set(CMAKE_CXX_COMPILER "/usr/bin/arm-none-eabi-g++")
 
-add_compile_options("-mcpu=cortex-m0")
+add_compile_options(
+  "-mcpu=cortex-m0"
+  "-Os"
+  "-fno-exceptions"
+  "-fno-unwind-tables"
+  "-ffunction-sections"
+  "-fdata-sections"
+)
 add_link_options("--specs=nosys.specs")
